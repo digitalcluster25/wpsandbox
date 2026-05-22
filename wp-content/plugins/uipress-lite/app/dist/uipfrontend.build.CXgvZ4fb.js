de268beff29b008a08875fc68cb0a6df8d3402d6
digitalcluster25 <digitalcluster25@gmail.com>
2026-05-22 04:50:03 +0000

    auto: db files[2026-05-22 04:50]

--- wp-content/plugins/uipress-lite/app/dist/uipfrontend.build.CXgvZ4fb.js
+++
@@ -1,2 +0,0 @@
-import{s as e}from"./assets/setupApp-C9QwgUBi.js";import"./assets/index-BrOX-PK1.js";const o=document.querySelector("#wpadminbar");if(o){const t=document.createElement("div");t.id="uip-frontend-toolbar",o.appendChild(t),e(t)}
-//# sourceMappingURL=uipfrontend.build.CXgvZ4fb.js.map
