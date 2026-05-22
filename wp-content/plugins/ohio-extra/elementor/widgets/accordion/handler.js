662aa8e8267ce08611ff3c7438f6e69efecbb3ba
digitalcluster25 <digitalcluster25@gmail.com>
2026-05-22 09:00:02 +0000

    auto: db files[2026-05-22 09:00]

--- wp-content/plugins/ohio-extra/elementor/widgets/accordion/handler.js
+++
@@ -1,8 +0,0 @@
-jQuery( window ).on('elementor/frontend/init', () => {
-    if (elementorFrontend.isEditMode()) {
-        elementorFrontend.hooks.addAction('frontend/element_ready/ohio_accordion.default', ( $element ) => {
-            jQuery(window).trigger('ohio:handle_accordion_box');
-            jQuery(window).trigger('ohio:handle_accordion_box_size');
-        });
-    }
-});
